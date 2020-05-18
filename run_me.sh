2758276e41665b9cc5a85723a5104af33b95eee8
Jakob G. Aaes <jakob1379@gmail.com>
2020-05-18 10:11:18 +0200

    echoes to see what operation is currently started and finished

--- run_me.sh
+++ run_me.sh
@@ -3,7 +3,21 @@
 
 for folder in $(ls -d */)
 do
-    bash submissions_unzip.sh "$folder"
+    echo "INFO: Downloading submissions"
+    python3 download_submissions.py
+    echo "INFO: Done!"
+
+    echo "INFO: Unzipping"
+    ./unzip_submissions2
+    echo "INFO: Done!"
+
+    echo "INFO: Correcting submissions"
     bash submissions_correcting.sh "$folder"
+    echo "INFO: Done!"
+
+    echo "INFO: zipping answers"
     bash submissions_zip.sh "$folder"
+    echo "INFO: Done"
 done
+
+echo "RUN ME: Done!"
