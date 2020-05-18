0effdb1235843c7639fff074a7e9becb4dd69cef
Jakob G. Aaes <jakob1379@gmail.com>
2020-05-18 10:29:03 +0200

    restructured run_me for redundancy

--- run_me.sh
+++ run_me.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 
+echo "INFO: Downloading submissions"
+python3 download_submissions.py
+echo "INFO: Done!"
+
+echo "INFO: Unzipping"
+./unzip_submissions2
+echo "INFO: Done!"
+
 for folder in $(ls -d */)
 do
-    echo "INFO: Downloading submissions"
-    python3 download_submissions.py
-    echo "INFO: Done!"
-
-    echo "INFO: Unzipping"
-    ./unzip_submissions2
-    echo "INFO: Done!"
-
     echo "INFO: Correcting submissions"
     bash submissions_correcting.sh "$folder"
     echo "INFO: Done!"
@@ -20,4 +20,4 @@ do
     echo "INFO: Done"
 done
 
-echo "RUN ME: Done!"
+echo "INFO: Done whole routine!"
