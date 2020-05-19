06cd1f57993ed203fb1ec8dce384bb59cab3cf20
Jakob G. Aaes <jakob1379@gmail.com>
2020-05-19 15:26:38 +0200

    modified to run in parallel with flag -p

--- run_me.sh
+++ run_me.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
+displayUsage() {
+    echo '
+<>  angle brackets for required parameters: ping <hostname>
+[]  square brackets for optional parameters: mkdir [-p] <dirname>
+... ellipses for repeated items: cp <source1> [source2…] <dest>
+ |  vertical bars for choice of items: netstat {-t|-u}
+
+usage:  name <operation> [...]
+operations:
+    name {-h help} shows this dialogue
+
+'
+}
+
+parallel=false
+while getopts ":hp" opt; do
+    case ${opt} in
+	p)
+	    echo "RU_NME: Parallelization enabled!"
+	    parallel=true
+	;;
+	h)
+	    displayUsage
+	    exit 1
+	    ;;
+	\?)
+	    echo "Invalid option: $OPTARG" 1>&2
+	    exit 2
+	    ;;
+	:)
+	    echo "Invalid option: $OPTARG requires an argument" 1>&2
+	    exit 2
+	    ;;
+    esac
+    shift
+done
 
 echo "INFO: Downloading submissions"
-python3 download_submissions.py
+$parallel && python3 download_submissions.py -p || python3 download_submissions.py
 echo "INFO: Done!"
 
 for folder in $(ls -d Week*/); do
@@ -11,7 +47,7 @@ for folder in $(ls -d Week*/); do
     echo "INFO: Done!"
 
     echo "INFO: Correcting submissions"
-    bash submissions_correcting.sh "$folder"
+    $parallel && bash submissions_correcting.sh -p "$folder" || bash submissions_correcting.sh "$folder"
     echo "INFO: Done!"
 
     echo "INFO: zipping answers"
