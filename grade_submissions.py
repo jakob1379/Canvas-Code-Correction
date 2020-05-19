13dcb3e0d019c135fd318f832f8df91f385ed1b4
Jakob G. Aaes <jakob1379@gmail.com>
2020-05-19 15:24:41 +0200

    refactored to be run in parallel with flag -p

--- grade_submissions.py
+++ grade_submissions.py
@@ -1,16 +1,22 @@
 # Import the Canvas class
 from canvasapi import Canvas
 from glob import glob
-import os
-import urllib.request
+from joblib import Parallel, delayed
+import argparse
+import multiprocessing
 import numpy as np
-import re
 import progressbar as Pbar
+import re
 
-def download_url(url, save_path):
-    with urllib.request.urlopen(url) as dl_file:
-        with open(save_path, 'wb') as out_file:
-            out_file.write(dl_file.read())
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--parallel",
+                    help="grade submissions in parallel",
+                    action='store_true')
+parser.add_argument("-v", "--verbose",
+                    help="set verbose",
+                    action='store_true')
+args = parser.parse_args()
+
 
 def file_to_string(file_name):
     with open(file_name) as f:
@@ -18,8 +24,52 @@ def file_to_string(file_name):
     return content.strip()
 
 
+def grade_submission(sub):
+    global args
+    scores_to_complete = {
+        'Week1-2': 43,
+        'Week3-4': 50,
+        'Week5-6': 50,
+        'Week7-8': 50}
+    # Get assignment- and file name
+    assignment_name = sub.split('/')[0]
+    fname = sub.split('/')[-2]
+
+    # get points and user id
+    points = np.loadtxt(glob(sub + fname + '_points.txt')[0]).sum()
+    user_id = re.findall(r'\d+', fname)[0]
+
+    # Get submission for user
+    submission = assignment.get_submission(user_id)
+
+    # Grade accordingly
+
+    out_str = 'Checking: ' + sub
+    if submission.grade == 'complete':
+        if args.verbose:
+            print(out_str)
+            print("Already passed!")
+            print()
+    elif (points >= scores_to_complete[assignment_name] and
+          submission.grade != 'complete'):
+        if args.verbose:
+            print(out_str)
+            print("Completed with points:", points)
+            print()
+        submission.edit(submission={'posted_grade': 'complete'})
+    elif (points < scores_to_complete[assignment_name] and
+          submission.grade != 'incomplete' and
+          submission.grade != 'complete'):
+        if args.verbose:
+            print(out_str)
+            print("Incomplete with points:", points)
+            print()
+        # submission.edit(submission={'posted_grade': 'incomplete'})
+
+
 # %% Init
-print('Initialising canvas...')
+if args.verbose:
+    print('Initialising canvas...')
 
 # Canvas API URL
 domain = 'absalon.ku.dk'
@@ -32,50 +82,22 @@ API_KEY = file_to_string('token')
 canvas = Canvas(API_URL, API_KEY)
 
 # init course
-course_id = file_to_string('course-id')
+course_id = file_to_string('course_id')
 course = canvas.get_course(course_id)
 
-# get users
+# get users assignment and local points
 users = course.get_users()
-
-
-
 assignment = course.get_assignments()[0]
-reports = sorted(glob(base_dir + 'Week*/submissions/*/'))
+reports = sorted(glob('Week*/submissions/*/'))
 
-score_to_complete = 43
-scores_to_complete = dict(1=43, 2=50, 3=50, 4=50)
-# pbar = Pbar.ProgressBar(redirect_stdout=True)
-# for rep in pbar(reports):
-for rep in reports:
-    fname = rep.split('/')[-1]
-
-    # get points and user id
-    points = np.loadtxt(glob(rep+'/*point*')[0]).sum()
-    file_to_upload = glob(rep+'/*.zip')[0]
-    user_id = re.findall(r'\d+', fname)[0]
-
-    # Get submission for user
-    submission = assignment.get_submission(user_id)
-
-    # Grade accordingly
-
-    # out_str = 'Checking: ' + fname
-    # if submission.grade == 'complete':
-    #     print(out_str)
-    #     print("Already passed!")
-    #     continue
-    # elif points >= score_to_complete and submission.grade != 'complete':
-    #     print(out_str)
-    #     print("Completed with points:", points)
-    #     print()
-    #     submission.edit(submission={'posted_grade': 'complete'})
-    # elif points < score_to_complete and (submission.grade != 'incomplete' and
-    #                       submission.grade != 'incomplete'):
-    #     print(out_str)
-    #     print("Incomplete with points:", points)
-    #     print()
-    #     submission.edit(submission={'posted_grade': 'incomplete'})
-
-
-    break
+# Let's start grading!
+pbar = Pbar.ProgressBar(redirect_stdout=True)
+num_cores = multiprocessing.cpu_count()
+if args.parallel:
+    if args.verbose:
+        print("Grading in parallel!")
+    Parallel(n_jobs=num_cores)(delayed(
+        grade_submission)(rep) for rep in pbar(reports))
+else:
+    for rep in pbar(reports):
+        grade_submission(rep)
