596621de71f8c74234e339add8d7b838927d1369
Jakob G. Aaes <jakob1379@gmail.com>
2020-05-20 14:42:20 +0200

    module to upload comments if they do not already exist

---
+++ upload_comments.py
@@ -0,0 +1,97 @@
+# Import the Canvas class
+from canvas_helpers import (file_to_string, create_file_name,
+                            flatten_list, print_dict)
+from canvasapi import Canvas
+from glob import glob
+from joblib import Parallel, delayed
+import argparse
+import multiprocessing
+import numpy as np
+import progressbar as Pbar
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--parallel",
+                    help="grade submissions in parallel",
+                    action='store_true')
+parser.add_argument("-v", "--verbose",
+                    help="set verbose",
+                    action='store_true')
+args = parser.parse_args()
+
+def extract_comment_filenames(comments):
+    # Get all attachments in comments as one flat list
+    return flatten_list(
+        [[att.get('filename') for att in comm.get('attachments')
+          if att.get('filename')]
+         for comm in comments if comm.get('attachments')])
+
+
+def upload_comments(sub, assignments, args):
+    # Get assignment- and file name
+    assignment_name = sub.split('/')[0]
+    handin_name = sub.split('/')[-2]
+
+    # get points and user id
+    user_id = re.findall(r'\d+', handin_name)[0]
+
+    # Get submission for user
+    assignment = assignments[assignment_name]
+    submission = assignment.get_submission(user_id,
+                                           include='submission_comments')
+
+    # extract attached filenames from comments
+    comment_files = extract_comment_filenames(submission.submission_comments)
+
+    out_str = 'Checking: ' + sub
+    # Only upload if it isn't already there.
+    if handin_name+'.zip' not in comment_files:
+        if args.verbose:
+            print(out_str)
+            print("Upload: uploading feedback\n")
+        basename = handin_name.split('_')[-1]
+        file_to_upload = glob(sub + basename + '.zip')[0]
+        submission.upload_comment(file_to_upload)
+    elif args.verbose:
+        print(out_str)
+        print("Upload: feedback already uploaded\n")
+
+
+# %% Init
+if args.verbose:
+    print('Initialising canvas...')
+
+# Canvas API URL
+domain = 'absalon.ku.dk'
+API_URL = "https://"+domain+"/"
+
+# Canvas API key
+API_KEY = file_to_string('token')
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
+
+# init course
+course_id = file_to_string('course_id')
+course = canvas.get_course(course_id)
+assignments_as_dict = {ass.name.capitalize().replace(' ', ''): ass
+                       for ass in course.get_assignments()}
+
+# get users and local points
+users = course.get_users()
+reports = sorted(glob('Week*/submissions/*/'))
+
+# Let's start grading!
+pbar = Pbar.ProgressBar(redirect_stdout=True)
+num_cores = multiprocessing.cpu_count()
+
+if args.parallel:
+    if args.verbose:
+        print("Uploading comments in parallel!")
+    Parallel(n_jobs=num_cores)(delayed(
+        upload_comments)(rep, assignments_as_dict, args) for
+                               rep in pbar(reports))
+else:
+    for rep in pbar(reports):
+        upload_comments(rep, assignments_as_dict, args)
+        break
