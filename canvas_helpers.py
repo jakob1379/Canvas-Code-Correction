2ae482e6a836233f62aed3e440e26b661e564c6d
Jakob G. Aaes <jakob1379@gmail.com>
2020-05-22 08:08:21 +0200

    added bcolors string class to color strings - remember to bcolor.ENDC to end coloring

--- canvas_helpers.py
+++ canvas_helpers.py
@@ -11,6 +11,18 @@ import re
 import shutil
 import urllib.request
 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def download_url(url, save_path):
     with urllib.request.urlopen(url) as dl_file:
         with open(save_path, 'wb') as out_file:
