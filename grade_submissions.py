ea1cb1fa659dc85ad052c523da08a5857ef91683
Jakob G. Aaes <jakob1379@gmail.com>
2020-05-27 14:14:56 +0200

    simplified logic and now all submissions graded, not just if the grading changes

--- grade_submissions.py
+++ grade_submissions.py
@@ -57,6 +57,15 @@ def grade_submission(sub, assignments, args):
 
     if args.verbose:
         print(out_str)
+
+    if not args.grade_all and submission.grade_matches_current_submission:
+        if args.verbose:
+            print(out_str)
+            print("Grading: Submission already graded")
+        return
+
+
+    # %% Print question and retrieve answer
     if args.question:
         score = round(points - scores_to_complete[assignment_name])
         if score < 0:
@@ -72,74 +81,60 @@ def grade_submission(sub, assignments, args):
         print("\nPoints to complete", assignment_name+':',
               scores_to_complete[assignment_name])
         print("Current grade:", current_grade)
-        ans = str(input("Grade the student?: [y/N/(o)verride]: ") or 'n').lower()
+        ans = str(input("Grade the student?: [y/N]: ") or 'n').lower()
         if ans == 'n':
             if args.verbose:
                 print("Submission not graded\n")
             return
-    if submission.grade == 'complete' and not args.grade_all:
-        if args.verbose:
-            print("Already passed!")
-            print()
-    elif points >= scores_to_complete[assignment_name] and (
-            submission.grade != 'complete' or args.grade_all or ans == 'o'):
+
+    # %% Edit online grade based on score and/or question answer if any
+    if points >= scores_to_complete[assignment_name] or ans == 'o':
         submission.edit(submission={'posted_grade': 'complete'})
-        if ans == 'o':
-            print("GRADE OVERWRITTEN!")
         if args.verbose:
-            print(out_str)
             print("Completed with points:", points)
-            print()
-    elif points < scores_to_complete[assignment_name] and (
-          submission.grade != 'incomplete' or args.grade_all or ans == 'o'):
+    elif points < scores_to_complete[assignment_name] or ans == 'o':
         submission.edit(submission={'posted_grade': 'incomplete'})
-        if ans == 'o':
-            print("GRADE OVERWRITTEN!")
         if args.verbose:
-            print(out_str)
             print("Incomplete with points:", points)
-            print()
-    elif points < scores_to_complete[assignment_name] and submission.grade != 'incomplete':
-        if args.verbose:
-            print("Already failed")
-
-
+    if args.verbose:
+        print()
 
 
 # %% Init
-if args.verbose:
-    print('Initialising canvas...')
-
-# Canvas API URL
-domain = 'absalon.ku.dk'
-API_URL = "https://"+domain+"/"
-
-# Canvas API key
-API_KEY = file_to_string('token')
-
-# Initialize a new Canvas object
-canvas = Canvas(API_URL, API_KEY)
-
-# init course
-course_id = file_to_string('course_id')
-course = canvas.get_course(course_id)
-assignments_as_dict = {ass.name.capitalize().replace(' ', ''): ass
-                       for ass in course.get_assignments()}
-
-# get users and local points
-users = course.get_users()
-reports = [rep for rep in sorted(glob(args.path)) if 'Week7-8' not in rep]
-
-# Let's start grading!
-# pbar = Pbar.ProgressBar(redirect_stdout=True)
-num_cores = multiprocessing.cpu_count()
-
-if args.parallel:
+if __name__ == '__main__':
     if args.verbose:
-        print("Grading: runnning in parallel!")
-    Parallel(n_jobs=num_cores)(delayed(
-        grade_submission)(rep, assignments_as_dict, args)
-                               for rep in reports)
-else:
-    for rep in reports:
-        grade_submission(rep, assignments_as_dict, args)
+        print('Initialising canvas...')
+
+    # Canvas API URL
+    domain = 'absalon.ku.dk'
+    API_URL = "https://"+domain+"/"
+
+    # Canvas API key
+    API_KEY = file_to_string('token')
+
+    # Initialize a new Canvas object
+    canvas = Canvas(API_URL, API_KEY)
+
+    # init course
+    course_id = file_to_string('course_id')
+    course = canvas.get_course(course_id)
+    assignments_as_dict = {ass.name.capitalize().replace(' ', ''): ass
+                           for ass in course.get_assignments()}
+
+    # get users and local points
+    users = course.get_users()
+    reports = [rep for rep in sorted(glob(args.path)) if 'Week7-8' not in rep]
+
+    # Let's start grading!
+    # pbar = Pbar.ProgressBar(redirect_stdout=True)
+    num_cores = multiprocessing.cpu_count()
+
+    if args.parallel:
+        if args.verbose:
+            print("Grading: runnning in parallel!")
+            Parallel(n_jobs=num_cores)(delayed(
+                grade_submission)(rep, assignments_as_dict, args)
+                                       for rep in reports)
+    else:
+        for rep in reports:
+            grade_submission(rep, assignments_as_dict, args)
