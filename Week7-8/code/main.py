b9ef926ba4b50d4fb964ffceaf374d9ea10dd292
Vulpesdk <emailkonto.dk@gmail.com>
2020-06-10 10:51:43 +0200

    Week7-8 restructuring

--- Week7-8/code/main.py
+++ Week7-8/code/main.py
@@ -1,4 +1,4 @@
-from config import * 
+from config import *
 import subprocess
 import glob
 import os
@@ -48,7 +48,7 @@ def extractStandardCompile():
 
 def run(foldername):
 
-    test_files_compiled = [True for i in xrange(len(test_files))] 
+    test_files_compiled = [True for i in xrange(len(test_files))]
     valggrind_logs = [test_files[i] + ".valgrind.txt" for i in xrange(len(test_files))]
 
     ## check if all files are present
@@ -74,7 +74,7 @@ def run(foldername):
             test_files_compiled[i] = False
             print "could not compile: " + line
             print "------------------------------------------"
- 
+
     ## run files with valggrind
     reachedTimeLimit = [False for i in xrange(len(test_files))]
     for i in xrange(len(test_files)):
@@ -90,109 +90,14 @@ def run(foldername):
                 myproces.kill()
                 reachedTimeLimit[i] = True
 
-            ##subprocess.call("valgrind  --log-file='" + valggrind_logs[i] + 
-            ##    "' ./" + object_files[test_files[i]] + " > deleteme.txt", shell=True)
+    # clean()
+    # f = open(foldername+ ".txt","w")
+    # f.write(".")#+outputFile)
+    # f.close()
 
-    ## analyze result files from valggrind
-    '''
-    result_files = [test_files[i] + ".valgrind.txt" for i in xrange(len(test_files))]
-    test_files_memory_freed = [True for x in xrange(len(test_files))]
-    test_files_segfault = [False for x in xrange(len(test_files))]
-    count = 0
-    for result in result_files:
-        if test_files_compiled[count] and not reachedTimeLimit[count]:
-            with open(result) as f:
-                content = f.readlines()
-                (test_files_memory_freed[count],test_files_segfault[count]) = memoryFreed_segfaulted(content)
-        count+=1
-
-    ## analyse result files from the tests and generate resulting file
-    outputFile = ""
-    points = [0 for i in xrange(len(result_files))]
-    result_files = [test_files[i] + ".result.txt" for i in xrange(len(test_files))]
-    allResultFiles = glob.glob("*.result.txt");
-
-    currentTestNumber = 0
-    points_earned = 0
-    for result in result_files:
-        outputFile+= head_lines[test_files[currentTestNumber]] + "\n"
-        if reachedTimeLimit[currentTestNumber]:
-            outputFile+= "-Time limit of " + str(TIME_LIMIT) + " seconds was reached - check for very bad code or infite loops.\n"
-            points[currentTestNumber] = 0
-        else:
-            # check if segfault occured
-            if test_files_segfault[currentTestNumber]:
-                outputFile+= "-Segfault occured.\n"#" You missed " + str(total_points[test_files[currentTestNumber]]) + " points\n"
-                points[currentTestNumber] = 0
-            # check if file compiled
-            elif not test_files_compiled[currentTestNumber]:
-                outputFile+= "-Code could not compile in testing framework (errors in code, perhaps it has a main function, or file has the wrong name).\n"#" You missed " + str(total_points[test_files[currentTestNumber]]) + " points\n"
-                points[currentTestNumber] = 0
-            # check if result file exists (was created)
-            elif result not in allResultFiles:
-                outputFile+= "-Something forced the code to exit prematurely (e.g. assertions or exit).\n"#" You missed " + str(total_points[test_files[currentTestNumber]]) + " points\n"
-                points[currentTestNumber] = 0
-            # open result file
-            else:            
-                with open(result) as f:
-                    content = f.readlines()
-                    #print content
-
-                    current = 0 
-                    allPassed = True
-                    while current < len(content):
-                        (passed, description) = testPassed(content[current], content[current+1])
-                        #print str(passed) + " : " + description
-                        allPassed = allPassed and passed
-                        if not passed:
-                            outputFile+= description + "\n"
-                        current+=2
-
-                    testsRunned = current/2
-                    # check if all the tests were run, and not ended early by something in the submission code
-                    if testsRunned != numberOfSubtests[test_files[currentTestNumber]]:
-                        outputFile+= "-Something forced the code to exit prematurely (e.g. assertions or exit).\n"#" You missed " + str(total_points[test_files[currentTestNumber]]) + " points\n"
-                    # check if all tests passed
-                    elif allPassed:
-                        # check if memory was freed correctly
-                        if test_files_memory_freed[currentTestNumber]:
-                            outputFile+= "-No errors found.\n"#" You earned " + str(total_points[test_files[currentTestNumber]]) + " points\n"
-                            points_earned+= total_points[test_files[currentTestNumber]]
-                            points[currentTestNumber] = total_points[test_files[currentTestNumber]]
-                        else:
-                            outputFile+= "-No errors found, but memory was leaked.\n"#" You earned " + str(total_points[test_files[currentTestNumber]]- minus_point_for_memory_leak) + " points\n"
-                            points_earned+= total_points[test_files[currentTestNumber]]*(1.0-minus_point_for_memory_leak)
-                            points[currentTestNumber] = total_points[test_files[currentTestNumber]]*(1-minus_point_for_memory_leak)
-                    else:
-                        if test_files_memory_freed[currentTestNumber]:
-                            outputFile+= "-Errors found.\n"#" You missed " + str(total_points[test_files[currentTestNumber]]) + " points\n"
-                        else:
-                            outputFile+= "-Errors found, and memory was leaked.\n"#" You missed " + str(total_points[test_files[currentTestNumber]]) + " points\n"
-                        points[currentTestNumber] = 0
-
-        outputFile+="\n"
-        currentTestNumber+=1
-    '''
-    #return 0
-    #outputFile += "You got " + str(round(float(sum(points))/sum(total_points.values())*100,2) ) + "% correct.\n"
-    #outputFile+="total points: " + str(sum(points)) + "\n"
-    #print outputFile
-    #print pointFile
-
-    #filefound, stdflag = extractStandardCompile()
-    #if filefound:
-    #    outputFile = "Compiled with what you supplied in standard.txt:" + stdflag + "\n\n" + outputFile
-    #else:
-    #    outputFile = "You did not supply a standard to compile with, so per default" + stdflag + " was used\n\n" + outputFile
-
-    clean()
-    f = open(foldername+ ".txt","w")
-    f.write(".")#+outputFile)
-    f.close()
-
-    f = open(foldername+ "_points.txt","w")
-    f.write("0.0")#+"".join([str(s)+"\n" for s in points]))
-    f.close()
+    # f = open(foldername+ "_points.txt","w")
+    # f.write("0.0")#+"".join([str(s)+"\n" for s in points]))
+    # f.close()
 
 def clean():
     with open(os.devnull, 'w')  as FNULL:
@@ -200,7 +105,7 @@ def clean():
             subprocess.call("rm " + fil, shell=True, stdout=FNULL, stderr=FNULL);
         for fil in object_files.values():
             subprocess.call("rm " + fil, shell=True, stdout=FNULL, stderr=FNULL);
-        subprocess.call("rm *.dat", shell=True, stdout=FNULL, stderr=FNULL);
+        # subprocess.call("rm *.dat", shell=True, stdout=FNULL, stderr=FNULL);
         subprocess.call("rm *.pyc", shell=True, stdout=FNULL, stderr=FNULL);
 
 def testPassed(title, result):
@@ -212,21 +117,10 @@ def testPassed(title, result):
         description+= "-Failed test: " + newtitle.strip()
     return (passed, description)
 
-def memoryFreed_segfaulted(lines):
-    memoryFree = False
-    segfaulted = False
-    checkStrMem = "All heap blocks were freed -- no leaks are possible"
-    checkStrSegfault = "Process terminating with default action of signal 11 (SIGSEGV)"
-    for line in lines:
-        if checkStrMem in line:
-            memoryFree = True
-        if checkStrSegfault in line:
-            segfaulted = True
-    return (memoryFree, segfaulted)
 
 if __name__ == '__main__':
     path = os.getcwd()
     folders = path.split("/")
     #print folders
     run(folders[-1]);
-    
+
