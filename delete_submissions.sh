e684650b3f4421396375d0bbfdccc68f02752974
jakob <jakob1379@gmail.com>
2021-06-17 12:21:39 +0200

    Simply deletes everything inside submission folder

--- delete_submissions.sh
+++ delete_submissions.sh
@@ -2,14 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [ $# -lt 1 ]
-then
-    folder=Week*/submissions/*/
-elif [ $# -eq 1 ] && [ -d $1 ]
-then
-     folder=$1
-else
-    echo "ERROR: Takes at most one argument!"
-fi
-
+folder=Week*/submissions/*
 rm -rf $folder
