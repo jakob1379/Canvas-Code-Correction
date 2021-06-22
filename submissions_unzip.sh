88120c73007db6dce898f4ee056a5b8633557ce0
jakob <jakob1379@gmail.com>
2021-06-22 12:35:02 +0200

    general improvement

--- submissions_unzip.sh
+++ submissions_unzip.sh
@@ -1,49 +1,40 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
+#!/bin/bash
 
-if [ $# -eq 0 ]
+if [ $# -ne 1 ] || [ ! -d $1 ]
 then
-    # if no args find
-    folders=$(find -type d -wholename "*submissions" | grep -oP "\w+.*/")
-else
-    folders="$@"
+    echo "submissions_unzip: Needs exactly one directory"
+    exit 1
 fi
-top_dir=$PWD
-for folder in $folders; do
-    # Check if folder exists
-    if [ ! -d "$folder" ]; then
-	continue
-    fi
 
-    # Enter folder and start unzipping
-    cd $folder
-    if [ -f submissions*.zip ]; then
-	unzip -q -n submissions*.zip -d submissions;
-	rm -f submissions*.zip
-    fi
+folder=$1
 
-    # rm zip files after unpacking
-    zipCount=$(ls -1 submissions/*.zip 2>/dev/null | wc -l)
-    if [ $zipCount -gt 0 ]; then
-	# To extract all zips:
-	for i in submissions/*.zip; do unzip -q -n "$i" -d "${i%%.zip}"; done
-	rm -f submissions/*.zip
-    fi
+cd "$folder"
 
-    # find files and move to top folder
-    for d in submissions/*/; do
-	if [ -d "__MACOSX" ]; then
-	    rm -rf "__MACOSX";
-	fi
-	folder_count=$(find "$d"/* -maxdepth 0 -type d -print 2>/dev/null | wc -l)
-	if [ "$folder_count" -gt "0" ] ; then
-	    mv "$d"*/*.hpp "$d"
-	    mv "$d"*/*.h "$d"
-	    mv "$d"*/*.cpp "$d"
-	    rm -fr "$d"*/;
-	fi
+if [ -f submissions*.zip ]; then
+    unzip -q -n submissions*.zip -d submissions;
+    rm -f submissions*.zip
+fi
+
+zipCount=$(ls -1 submissions/*.zip 2>/dev/null | wc -l)
+if [ $zipCount -gt 0 ]; then
+    # To extract all zips:
+    for i in submissions/*.zip; do
+	unzip -q -n "$i" -d "${i%%.zip}";
     done
+    rm -f submissions/*.zip
+fi
 
-    cd $top_dir
+
+# # find files and move to top folder
+for d in submissions/*/; do
+    if [ -d "__MACOSX" ]; then
+	rm -rf "__MACOSX";
+    fi
+    folder_count=$(find "$d"/* -maxdepth 0 -type d -print 2>/dev/null | wc -l)
+    if [ "$folder_count" -gt "0" ] ; then
+	mv "$d"*/*.hpp "$d"
+	mv "$d"*/*.h "$d"
+	mv "$d"*/*.cpp "$d"
+	rm -fr "$d"*/;
+    fi
 done
