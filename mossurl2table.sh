6b39288afc224567d50c101219baf233e7ffa6ff
jakob <jakob1379@gmail.com>
2021-08-04 18:17:23 +0200

    plagiarism tools - not intended for autorun just yet

---
+++ mossurl2table.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ $# -ne 1 ]
+then
+    echo "mossurl2table ERROR: Take only one input!"
+    exit 2
+else
+    # Check if url is seemingly valid
+    url="$1"
+    substring="http://moss.stanford.edu/results"
+    if [ ! -z "${url##*$substring*}" ]; then
+	echo "invalid url!"
+	exit 2
+    fi
+fi
+
+curl -s "$url" \
+    | hxnormalize -x \
+    | hxselect 'table' \
+    | w3m -dump -cols 2000 -T 'text/html' \
+    | awk '(NR>1) {print $1,$3,$2}' \
+    | sed -e 's/%//' -e 's/(//' -e 's/)//' \
+    | sort -k 3,3 -k 1,1 -h -r
