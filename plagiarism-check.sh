d055a35182160fb5482a22c2c221d1f724abcfd1
jakob <jakob1379@gmail.com>
2021-08-05 05:08:56 +0200

    finally got moss to work properly by inserting backslashes in path

--- plagiarism-check.sh
+++ plagiarism-check.sh
@@ -9,29 +9,43 @@ then
 fi
 
 # folders="$(find -wholename "*/submissions")"
-extensions=$(awk -F '=' -e '/^ext/{print $2}' config.ini | sed "s/'//g")
-cutoff=$(awk -F '=' '/cutoff/{print $2}' config.ini)
+extensions=$(awk -F '=' -e '/^ext/{print $2}' config.ini)
+cutoff=$(awk -F '=' 'BEGIN{ORS=""}/cutoff/{print $2}' config.ini)
+language=$(awk -F '=' 'BEGIN{ORS=""}/lang/{print $2}' config.ini )
 
 function check_assignment {
     # create input for moss
-    assignment=$(basename $1)
-
+    assignment=$(basename "$1")
+    mossPath=$(basename "$1" | sed 's/ /\\ /g')
     # # download all the handins
     # rm -rf "$assignment/submissions/*"
     # python download_submissions.py -d all -a "$assignment"
     # bash submissions_unzip.sh "$assignment/"
 
     # Construct the paths for moss by checking each presence of each extension
-    paths_to_check="$(for ext in $extensions; do [ ! -z  "$(find "$assignment/submissions" -type f -name "*$ext")" ] && echo "$assignment/submissions/*/*$ext"; done)"
+    paths_to_check=()
+    echo "checking extensions..."
+    for ext in $extensions
+    do
+	if [ ! -z  "$(find "$assignment/submissions" -type f -name "*$ext")" ]
+	then
+	    # paths_to_check="$paths_to_check""$mossPath/submissions/*/*$ext "
+	    paths_to_check+=("$mossPath/submissions/*/*$ext")
+	fi
+    done
 
     # Catch result url
+
     echo "uploading to moss..."
-    url="$(moss -d $paths_to_check | grep -P 'http://moss.stanford.edu/results.*')""/"
-    # url="http://moss.stanford.edu/results/8/4255768150604/" # a test url
+    # url=$(eval "moss -d $paths_to_check" | grep -P 'http://moss.stanford.edu/results.*')/
+    url=$(./moss -l $language -d ${paths_to_check[@]} | grep -oP 'http://moss.stanford.edu/results.*')/
+    echo "$url"
+    # # url="http://moss.stanford.edu/results/8/4255768150604/" # a test url
 
     # Create table for local inspection
     echo "creating table..."
-    bash mossurl2table.sh "$url" > "$assignment/similarity.txt"
+    echo "$url" > "$assignment/similarity.txt"
+    bash mossurl2table.sh "$url" >> "$assignment/similarity.txt"
 
     # Print lines that crosses the cutoff value
     suspecious_handins=$(awk -v cutoff="$cutoff" '{ if ($3 >= cutoff) { print } }' "$assignment/similarity.txt")
@@ -47,7 +61,7 @@ function check_assignment {
 	mossum \
 	    --min-percent "$cutoff" \
 	    --format pdf \
-	    --transformer '.*/submissions/(.*)/' \
+	    --transformer '.*/submissions/(.*)_.*_.*_(.*).*/' \
 	    --output "$assignment/similarity_graph" \
 	    "$url"
     fi
@@ -56,6 +70,6 @@ function check_assignment {
 # combine folders and extensions into regexes for moss
 for folder in $@; do
     echo "Checking: $folder"
-    check_assignment $folder &
+    check_assignment "$folder" &q
 done
 wait
