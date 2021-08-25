85a9be4b2a601649c278c9ec2df631d7749e0cc8
jakob <jakob1379@gmail.com>
2021-08-25 13:31:35 +0200

    echoes assignment names

--- setup-assignment-folders.sh
+++ setup-assignment-folders.sh
@@ -5,5 +5,6 @@ IFS=$'\n\t'
 assignmentNames=$(python download_submissions.py -l | grep -P ' - ' | sed 's/ - //')
 
 for name in $assignmentNames; do
+    echo "$name"
     mkdir -p "$name/code"
 done
