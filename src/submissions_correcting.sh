de32489549a67d861130c3753da79bf842d6e587
jakob <jakob1379@gmail.com>
2021-09-24 10:55:46 +0200

    fix: try with altered exit_code writing

--- src/submissions_correcting.sh
+++ src/submissions_correcting.sh
@@ -100,7 +100,7 @@ function correction_routine {
 	if [ "$sandbox" == 'yes' ]; then
 	    timeout $maxtime firejail sh main.sh 2> /dev/null
 	else
-	    timeout $maxtime sh main.sh 2> /dev/null; exit_code="$?"
+	    timeout $maxtime sh main.sh 2> /dev/null && exit_code=0 ||  exit_code="$?"
 	fi
     fi
     echo "###### FINISHED RUNNING CODE ######## "
