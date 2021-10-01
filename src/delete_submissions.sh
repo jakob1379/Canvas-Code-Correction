a222f20b36c2f97eed025131411c1fcd786af0c3
jakob <jakob1379@gmail.com>
2021-10-01 11:54:51 +0200

    fix: using /usr/rm instead of find to delete

--- src/delete_submissions.sh
+++ src/delete_submissions.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # -depth enables processing of files before dir
-find -mindepth 3 -wholename "*/submissions/**" -exec rm -rf "{}" \;
+find -mindepth 3 -maxdepth 3 -wholename "*/submissions/*" | xargs /usr/bin/rm -rf
