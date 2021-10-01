03c7a459185504d612098a689763fe0e7eac35d6
jakob <jakob1379@gmail.com>
2021-10-01 10:33:38 +0200

    fix: more robust clearing of submissions

--- src/delete_submissions.sh
+++ src/delete_submissions.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # -depth enables processing of files before dir
-find -mindepth 3 -wholename "*/submissions/**" -delete
+find -mindepth 3 -wholename "*/submissions/**" -exec rm -rf "{}" \;
