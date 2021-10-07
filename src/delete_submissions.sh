048a0dfca031f854bef8d9c1f425daf6cb921a10
jakob <jakob1379@gmail.com>
2021-10-07 20:53:10 +0200

    style: suppresses warning from rm

--- src/delete_submissions.sh
+++ src/delete_submissions.sh
@@ -6,5 +6,5 @@ IFS=$'\n\t'
 to_delete=$(find -mindepth 3 -maxdepth 3 -wholename "*/submissions/*")
 
 for f in $to_delete;do
-    rm -rf "$f" || true #echo "Cannot delete: $f"
+    rm -rf "$f" 2> /dev/null || true #echo "Cannot delete: $f"
 done
