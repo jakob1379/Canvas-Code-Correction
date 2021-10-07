e3a6ddd20ea17abf844dbc9095e314d0444b948c
jakob <jakob1379@gmail.com>
2021-10-07 20:48:06 +0200

    fix: now handles unwritable files properly

--- src/submissions_unzip.sh
+++ src/submissions_unzip.sh
@@ -15,6 +15,7 @@ find 'submissions/' \
      -type f \
      -name '*.zip' \
      -exec sh -c 'unzip -q -n "{}" -d "$(dirname "{}")" && rm -rf "{}"' \;
+
 find 'submissions/' \
      -maxdepth 2 \
      -type d \
@@ -25,8 +26,15 @@ find 'submissions/' \
 for d in submissions/*/; do
     folder_count=$(find "$d"/* -maxdepth 0 -type d -print 2>/dev/null | wc -l)
     file_count=$(find "$d"/* -maxdepth 0 -type f -print 2>/dev/null | wc -l)
+    # Check if folder only has a single folder
     if [ "$folder_count" -eq "1" ] && [ "$file_count" -eq "0" ]; then
-	mv "$d"*/* "$d"
-	rm -fr "$d"*/ || true
+	# Only touch files that are writable
+	for thing in $(find "$d" -mindepth 2 -maxdepth 2); do
+	    if [[ "$(stat -c "%A" "$thing")" =~ 'w' ]]; then
+		destination=$(rev <(echo "$thing") | cut -d '/' -f 3- | rev)
+		mv "$thing" "$destination/"
+		rm -fr "$thing"
+	    fi
+	done
     fi
 done
